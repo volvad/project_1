7272e9e5ddaa601024d39403e2ed4598db29a103
volvad <volgvad@gmail.com>
2022-09-13 16:49:30 +0400

    Add js

--- index.php
+++ index.php
@@ -1,5 +1,18 @@
-<!DOCTYPE html>
+<?php
+
+$db = new PDO('sqlite:db/project_1.db');
+
+$statement = $db->query("SELECT * FROM recipes");
+
+$rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+var_dump($rows)
+
+?>
+
+<!--<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="css/style.css">
@@ -12,19 +25,18 @@
     </header>
 
     <div>
-        <h1 id="transform">TEST TEXT</h1>
+        <h1>TEST TEXT</h1>
     </div>
 
     <footer>
-        <!--h4><span>Contact Me</span></h4>
+        <h4><span>Contact Me</span></h4>
         <form>
             <input name="name" type="text"><br>
             <input name="email" type="email"><br>
             <textarea name="message"></textarea><br>
             <input type="submit" value="SEND" class="submit">
-        </form-->
+        </form>
     </footer>
-
 </body>
-<script src="js/script.js"></script>
-</html>
+
+</html>-->
