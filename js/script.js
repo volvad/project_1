cd0b1b15b35071cc7f97a22d2a113278bb3e8f9f
volvad <volgvad@gmail.com>
2022-09-14 15:37:31 +0400

    JS training

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("All good, no error!");
+/* "js/script.js" for "index.html" */
+
+console.log("All good, no error!"); /* Print to console */
+
+//document.write("<p>Hello World</p>"); /* Print to document */
+
+//var x = "<p>100</p>"; /* Assign a value to a variable */
+//document.write(x); /* Print value to document */
+
+//alert("This is an alert box!"); /* Alert window */
+
+//var x = "Hello"; /* Variable one */
+//var y = "World"; /* Variable two */
+//var z = x+"&nbsp;&nbsp;&nbsp"+y; /* Sum and non-breaking space */
+//document.write("<h1>"); /* Print h1 to document */
+//document.write(z); /* Print sum to document */
+//document.write("</h1>"); /* Print /h1 to document */
+
